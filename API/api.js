585ec7751d931a1016a2a34680f54d4b8c495922
Ankit <111271551+codewithankit047@users.noreply.github.com>
2023-07-11 23:34:01 +0530

    infor

--- API/api.js
+++ API/api.js
@@ -3,13 +3,18 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
+/* 
+get method ------> to get the data from the  database through api
+*/
 
 app.get("/", async (req, resp) => {
   let data = await dbConnection();
   data = await data.find().toArray();
   resp.send(data);
 });
-
+/* 
+post method ------> to send the data in database through api
+*/
 app.post("/", async (req, resp) => {
   let data = await dbConnection();
   let result = await data.insertOne(req.body);
@@ -17,4 +22,7 @@ app.post("/", async (req, resp) => {
   resp.send(result);
 });
 
+/* 
+put method ------> to update the data in database through api
+*/
 app.listen(7000);
