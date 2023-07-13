7aef1cb2eba1b851ac1f083240054c651aa11bd8
Ankit <111271551+codewithankit047@users.noreply.github.com>
2023-07-13 20:47:08 +0530

    delet and update api

---
+++ MongooseAPI/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+require("./config");
+const Product = require("./product");
+
+const app = express();
+app.use(express.json());
+
+app.post("/create", async (req, resp) => {
+  let data = new Product(req.body);
+  let result = await data.save();
+  console.log(result);
+  resp.send(result);
+  //   resp.send(req.body);
+});
+app.get("/get", async (req, resp) => {
+  let data = await Product.find();
+  resp.send(data);
+});
+app.delete("/delete/:_id", async (req, resp) => {
+  console.log(req.params);
+  let data = await Product.deleteOne(req.params);
+
+  resp.send("done");
+});
+app.put("/update/:_id", async (req, resp) => {
+  console.log(req.params);
+
+  let data = await Product.updateOne(
+    { _id: req.params },
+    {
+      $set: req.body,
+    }
+  );
+
+  resp.send(data);
+});
+
+app.listen(5000);
