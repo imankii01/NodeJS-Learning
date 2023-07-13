9dc9317ba7e019a67c3472dfbf59b1c9e9a0b26e
Ankit <111271551+codewithankit047@users.noreply.github.com>
2023-07-13 13:45:46 +0530

    crud operation

--- Mongoose/index.js
+++ Mongoose/index.js
@@ -1,5 +1,25 @@
 const mongoose = require("mongoose");
-const main = async () => {
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/e-comm", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
+
+const ProductSchema = new mongoose.Schema({
+  name: String,
+  price: Number,
+  brand: String,
+  category: String,
+});
+
+const saveInDB = async () => {
   await mongoose
     .connect("mongodb://127.0.0.1:27017/e-comm", {
       useNewUrlParser: true,
@@ -12,24 +32,47 @@ const main = async () => {
       console.error("Error connecting to MongoDB:", error);
     });
 
-  const ProductSchema = new mongoose.Schema({
-    name: String,
-    price: Number,
-    brand: String,
-    category: String,
-  });
   // create model from schema and export it to use in other files of
   const ProdcutsModel = mongoose.model("products", ProductSchema);
   let data = new ProdcutsModel({
-    name: "Ankit",
-    price: 1000,
-    brand: "macbook",
+    name: "Note pro",
+    price: 3000,
+    brand: "Mi Not",
     category: "mobile",
-    rollno: "sajhdf",
   });
 
   let result = await data.save();
   console.log(result);
 };
 
-main();
+const updateInDB = async () => {
+  const Prodcuts = mongoose.model("products", ProductSchema);
+
+  let data = await Prodcuts.updateOne(
+    { name: "m8" },
+    {
+      $set: { price: 700 },
+    }
+  );
+  console.log(data);
+};
+const deleteInDb = async () => {
+  const Prodcuts = mongoose.model("products", ProductSchema);
+
+  let data = await Prodcuts.deleteMany({
+    name: "Ankit",
+  });
+
+  console.log(data);
+};
+const findInDb = async () => {
+  const Prodcuts = mongoose.model("products", ProductSchema);
+
+  let data = await Prodcuts.find({ name: "Note pro" });
+
+  console.log(data);
+};
+// deleteInDb();
+// updateInDB();
+
+findInDb();
