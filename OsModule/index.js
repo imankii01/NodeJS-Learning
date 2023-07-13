07eb44529102361ca4e232614723e9d28e2eb2e5
Ankit <111271551+codewithankit047@users.noreply.github.com>
2023-07-13 22:49:55 +0530

    os module

---
+++ OsModule/index.js
@@ -0,0 +1,9 @@
+const os = require("os");
+// console.log(os.arch());  to give the bits value
+// freemem() //to give the ream storage in the form of butes
+// console.log(os.freemem() / (1024 * 1024 * 1024));
+// hostname() to chheck the hostname of the pc
+// console.log(os.totalmem() / (1024 * 1024 * 1024));
+// console.log(os.hostname());
+// console.log(os.platform());
+console.log(os.userInfo());
