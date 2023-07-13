2386fd463a7853cf612cd716bddfdfc36ec25813
Ankit <111271551+codewithankit047@users.noreply.github.com>
2023-07-13 21:17:01 +0530

    search api

--- MongooseAPI/index.js
+++ MongooseAPI/index.js
@@ -3,6 +3,7 @@ require("./config");
 const Product = require("./product");
 
 const app = express();
+
 app.use(express.json());
 
 app.post("/create", async (req, resp) => {
@@ -10,18 +11,20 @@ app.post("/create", async (req, resp) => {
   let result = await data.save();
   console.log(result);
   resp.send(result);
-  //   resp.send(req.body);
 });
+
 app.get("/get", async (req, resp) => {
   let data = await Product.find();
   resp.send(data);
 });
+
 app.delete("/delete/:_id", async (req, resp) => {
   console.log(req.params);
   let data = await Product.deleteOne(req.params);
 
   resp.send("done");
 });
+
 app.put("/update/:_id", async (req, resp) => {
   console.log(req.params);
 
@@ -35,4 +38,22 @@ app.put("/update/:_id", async (req, resp) => {
   resp.send(data);
 });
 
+app.get("/search/:key", async (req, resp) => {
+  console.log(req.params.key);
+  let data = await Product.find({
+    $or: [
+      {
+        name: { $regex: req.params.key },
+      },
+      {
+        brand: { $regex: req.params.key },
+      },
+
+      {
+        category: { $regex: req.params.key },
+      },
+    ],
+  });
+  resp.send(data);
+});
 app.listen(5000);
