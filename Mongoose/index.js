a23aaaa1fa557c50fb0a0ca7a6968a01324e7de0
Ankit <111271551+codewithankit047@users.noreply.github.com>
2023-07-13 13:08:17 +0530

    mongoose connect

--- Mongoose/index.js
+++ Mongoose/index.js
@@ -1,17 +1,33 @@
 const mongoose = require("mongoose");
 const main = async () => {
-    await mongoose.connect('mongodb://localhost:27017/e-comm');
-    const productSchema = new mongoose.Schema({
-        name: String,
-        price: Number,
-        brand: String,
-        category: String
-    
+  await mongoose
+    .connect("mongodb://127.0.0.1:27017/e-comm", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("Connected to MongoDB");
+    })
+    .catch((error) => {
+      console.error("Error connecting to MongoDB:", error);
     });
+
+  const ProductSchema = new mongoose.Schema({
+    name: String,
+    price: Number,
+    brand: String,
+    category: String,
+  });
   // create model from schema and export it to use in other files of
-  const ProdcutsModel = mongoose.model("products", ProductsSchema);
-  let data = new ProdcutsModel({ name: "655" });
-  
+  const ProdcutsModel = mongoose.model("products", ProductSchema);
+  let data = new ProdcutsModel({
+    name: "Ankit",
+    price: 1000,
+    brand: "macbook",
+    category: "mobile",
+    rollno: "sajhdf",
+  });
+
   let result = await data.save();
   console.log(result);
 };
