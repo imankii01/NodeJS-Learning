7ad2028906aea4cbc381b821e52c76e33cc3fcc3
Ankit <private.ankit047@gmail.com>
2025-01-06 04:08:52 +0530

    Auto-commit at 1/6/2025, 4:08:52 AM

---
+++ dummy-2025-01-05T22-38-51-906Z.js
@@ -0,0 +1,15 @@
+
+// filepath: /Users/ankit/development/simple-git/repo/NodeJS-Learning/dummy-2025-01-05T22-38-51-906Z.js
+function multiply(a, b) {
+      return a * b;
+    }
+
+function subtract(a, b) {
+      return a - b;
+    }
+
+function add(a, b) {
+      return a + b;
+    }
+
+module.exports = { helloWorld, add, subtract, multiply, divide };
